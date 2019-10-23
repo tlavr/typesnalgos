a976a196bce8542bb5b15fd819e449dff286a31a
Timur Lavrukhin <45234552+tlavr@users.noreply.github.com>
2019-10-23 17:32:31 +0300

    changed linked list len function

--- queues.py
+++ queues.py
@@ -9,6 +9,7 @@ class LinkedList2:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
     def add_in_tail(self, item):
         if self.head is None:
@@ -19,6 +20,7 @@ class LinkedList2:
             self.tail.next = item
             item.prev = self.tail
         self.tail = item
+        self.length  += 1
 
     def add_in_head(self, newNode):
         newNode.next = None
@@ -29,6 +31,7 @@ class LinkedList2:
             newNode.next = self.head
             self.head.prev = newNode
             self.head = newNode
+            self.length  += 1
 
     def delete(self, val, all=False):
         node = self.head
@@ -38,6 +41,7 @@ class LinkedList2:
                     if node.prev is None:
                         self.head = node.next
                         self.head.prev = None
+                        self.length  -= 1
                         if all:
                             self.delete(val, True)
                         else:
@@ -45,6 +49,7 @@ class LinkedList2:
                     else:
                         node.next.prev = node.prev
                         node.prev.next = node.next
+                        self.length  -= 1
                         if all:
                             self.delete(val, True)
                         else:
@@ -53,20 +58,17 @@ class LinkedList2:
                     if node.prev is not None:
                         self.tail = node.prev
                         self.tail.next = None
+                        self.length  -= 1
                         return
                     else:
                         self.head = None
                         self.tail = None
+                        self.length  -= 1
                         return
             node = node.next
 
     def len(self):
-        ans = 0
-        node = self.head
-        while node is not None:
-            ans += 1
-            node = node.next
-        return ans
+        return self.length
 
 class Stack:
     def __init__(self):
