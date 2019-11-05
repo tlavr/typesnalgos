35f13407105174c365d3092ea01fbbf0e1b2742b
Timur Lavrukhin <45234552+tlavr@users.noreply.github.com>
2019-11-05 17:40:03 +0300

    hash tables

---
+++ hash_table_tests.py
@@ -0,0 +1,13 @@
+from hash_table import *
+
+ht = HashTable(17,4)
+
+print(ht.put('sadf'))
+print(ht.put('asfsafdsaf'))
+print(ht.put('aabyadb'))
+print(ht.put('sdfsdfa'))
+print(ht.put('fbasfsdb'))
+print(ht.put('sysayy'))
+print(ht.put('sdfsdafds'))
+
+print(ht.find('sysayy'))
