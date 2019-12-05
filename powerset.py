a342db468a6231fb1fdacf23eeb4ffbec7832ad5
Timur Lavrukhin <45234552+tlavr@users.noreply.github.com>
2019-12-05 12:34:38 +0300

    some sets

---
+++ powerset.py
@@ -0,0 +1,143 @@
+# наследуйте этот класс от HashTable
+# или расширьте его методами из HashTable
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        # в качестве value поступают строки!
+        # всегда возвращает корректный индекс слота
+        # всегда возвращает корректный индекс слота
+        hash = 0
+        for i in value:
+            hash = (hash*17 + ord(i)) % self.size
+        return hash # hash [0..self.size - 1]
+
+    def seek_slot(self, value):
+        # находит индекс пустого слота для значения, или None
+        idx = self.hash_fun(value)
+        idx0 = idx
+        while self.slots[idx] != None:
+            idx = (idx + self.step) % self.size
+            if idx == idx0:
+                return None
+        return idx
+
+    def put(self, value):
+        # записываем значение по хэш-функции
+        idx = self.seek_slot(value)
+        if idx is None:
+            return None
+        else:
+            self.slots[idx] = value
+            return idx
+        # возвращается индекс слота или None,
+        # если из-за коллизий элемент не удаётся
+        # разместить
+
+    def find(self, value):
+        # находит индекс слота со значением, или None
+        idx = self.hash_fun(value)
+        idx0 = idx
+        while self.slots[idx] != value:
+            idx = (idx + self.step) % self.size
+            if idx == idx0:
+                return None
+        return idx
+
+class PowerSet(HashTable):
+
+    def __init__(self):
+        self.hash = HashTable(20000,0)
+        self.len = 0
+        # ваша реализация хранилища
+
+    def size(self):
+        return self.len
+        # количество элементов в множестве
+
+    def put(self, value):
+        # всегда срабатывает
+        isput = self.hash.put(str(value))
+        if isput is not None:
+            self.len += 1
+
+    def get(self, value):
+        # возвращает True если value имеется в множестве,
+        # иначе False
+        value = str(value)
+        if self.hash.find(value) != None:
+            return True
+        return False
+
+    def remove(self, value):
+        # возвращает True если value удалено
+        # иначе False
+        value = str(value)
+        idx = self.hash.find(value)
+        if idx != None:
+            self.hash.slots[idx] = None
+            self.len -= 1
+            return True
+        return False
+
+    def intersection(self, set2):
+        # пересечение текущего множества и set2
+        if set2 is not None:
+            set_inter = PowerSet()
+            for item in set2.hash.slots:
+                if item != None:
+                    if self.get(item):
+                        set_inter.put(item)
+            if set_inter.size() is 0:
+                return None
+            return set_inter
+        return None
+
+    def union(self, set2):
+        # объединение текущего множества и set2
+        if set2 is not None:
+            set_union = PowerSet()
+            for item in set2.hash.slots:
+                if item is not None:
+                    set_union.put(item)
+            for item in self.hash.slots:
+                if item is not None:
+                    set_union.put(item)
+            if set_union.size() is 0:
+                return None
+            return set_union
+        return None
+
+    def difference(self, set2):
+        # разница текущего множества и set2
+        if set2 is not None:
+            set_dif = self.union(set2)
+            set_int = self.intersection(set2)
+            if set_dif is not None:
+                if set_int is not None:
+                    for item in set_dif.hash.slots:
+                        if item != None:
+                            if set_int.get(item):
+                                set_dif.remove(item)
+                if set_dif.size() is 0:
+                    return None
+            return set_dif
+        return None
+
+    def issubset(self, set2):
+        # возвращает True, если set2 есть
+        # подмножество текущего множества,
+        # иначе False
+        if set2 is not None:
+            if set2.size() <= self.size():
+                if set2.size() == 0:
+                    return True
+                set_int = self.intersection(set2)
+                if set_int is not None:
+                    if self.intersection(set2).size() == set2.size():
+                        return True
+            return False
+        return True
---
+++ powerset_tests.py
@@ -0,0 +1,86 @@
+from powerset import *
+
+def printset(set):
+    print('Items in the set:')
+    if set is not None:
+        for item in set.hash.slots:
+            if item != None:
+                print(item)
+        print('\n')
+
+set = PowerSet()
+
+# put tests []
+print('Put tests started!')
+print('Set size is: '+str(set.size()))
+try:
+    printset(set)
+    set.put(125)
+    printset(set)
+    set.put(125)
+    printset(set)
+    set.put('126')
+    printset(set)
+    set.put(125)
+    printset(set)
+except:
+    print('Put error!')
+
+#remove tests [125,126]
+print('Remove tests started!')
+print('Set size is: '+str(set.size()))
+try:
+    set.remove(125)
+    printset(set)
+    set.remove(127)
+    printset(set)
+except:
+    print('Remove error!')
+
+#intersection tests [126]
+print('Intersection tests started!')
+print('Set size is: '+str(set.size()))
+try:
+    set2 = PowerSet()
+    set2.put(126)
+    set2.put('128')
+    set3 = PowerSet()
+    set3.put(127)
+    set3.put(129)
+    printset(set.intersection(set2))
+    printset(set.intersection(set3))
+except:
+    print('Intersection error!')
+
+#union tests 1:[126] 2:[126,128] 3: [127,129]
+print('Union tests started!')
+print('Set size is: '+str(set.size()))
+try:
+    set4 = PowerSet()
+    printset(set.union(set2))
+    printset(set.union(set4))
+    printset(set.union(set3))
+except:
+    print('Union error!')
+
+#difference tests 1:[126] 2:[126,128] 3: [127,129] 4:[]
+print('Difference tests started!')
+print('Set size is: '+str(set.size()))
+try:
+    printset(set.difference(set4))
+    printset(set.difference(set2))
+    printset(set.difference(set3))
+except:
+    print('Difference error!')
+
+#subset tests 1:[126] 2:[126,128] 3: [127,129] 4:[]
+print('Subset tests started!')
+print('Set size is: '+str(set.size()))
+try:
+    print(set.issubset(set2))
+    print(set.issubset(set3))
+    print(set.issubset(set4))
+    print(set2.issubset(set))
+except:
+    print('Subset error!')
+
