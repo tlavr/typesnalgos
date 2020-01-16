2e4654f9e0e4d7c92b4971a00709a80091be80de
Timur Lavrukhin <45234552+tlavr@users.noreply.github.com>
2020-01-16 15:45:32 +0300

    balanced tree simple realisation. added tests

---
+++ bBST.py
@@ -0,0 +1,69 @@
+class BSTNode:
+    def __init__(self, key, parent):
+        self.NodeKey = key  # ключ узла
+        self.Parent = parent  # родитель или None для корня
+        self.LeftChild = None  # левый потомок
+        self.RightChild = None  # правый потомок
+        self.Level = 0  # уровень узла
+
+
+class BalancedBST:
+    def __init__(self):
+        self.Root = None  # корень дерева
+        self.allNodes = []
+
+    def makeTree(self, nodeParent, arr, lvl):
+        if arr.__len__() > 0:
+            centIdx = round(arr.__len__() / 2 - 0.1)
+            node = BSTNode(None, None)
+            node.NodeKey = arr[centIdx]
+            node.Parent = nodeParent
+            node.Level = lvl
+            node.LeftChild = self.makeTree(node, arr[:centIdx], lvl+1)
+            node.RightChild = self.makeTree(node, arr[centIdx + 1:], lvl+1)
+            return node
+        return None
+
+    def GenerateTree(self, a):
+    # создаём дерево с нуля из неотсортированного массива a
+        if a.__len__() == 0:
+            return None
+        # Sort array
+        a = sorted(a)
+        # Recursive add all elements
+        self.Root = self.makeTree(None, a, 0)  # parameters for the first adding
+
+    def detTreeLvl(self,node,lvl):
+        if node.Level > lvl:
+            lvl = node.Level
+        if node.LeftChild is not None:
+            lvl = self.detTreeLvl(node.LeftChild,lvl)
+        if node.RightChild is not None:
+            lvl = self.detTreeLvl(node.RightChild,lvl)
+        return lvl
+
+    def IsBalanced(self, root_node):
+        if abs(self.detTreeLvl(root_node.LeftChild,root_node.Level)-self.detTreeLvl(root_node.RightChild,root_node.Level)) < 2:
+            return True
+        return False  # сбалансировано ли дерево с корнем root_node
+
+    def __collectAllNodes__(self,fromNode = None, mode = 0):
+        if fromNode is None or fromNode is self.Root:
+            fromNode = self.Root
+            self.allNodes = []
+        if fromNode is None:
+            return
+        if mode == 2:
+            self.allNodes.append(fromNode)
+        if fromNode.LeftChild is not None:
+            self.__collectAllNodes__(fromNode.LeftChild)
+        if mode == 0:
+            self.allNodes.append(fromNode)
+        if fromNode.RightChild is not None:
+            self.__collectAllNodes__(fromNode.RightChild)
+        if mode == 1:
+            self.allNodes.append(fromNode)
+
+    def getAllNodes(self):
+        self.__collectAllNodes__()
+        return self.allNodes
--- bBST_tests.py
+++ bBST_tests.py
@@ -1,5 +1,28 @@
 from bBST_func import *
+from bBST import *
 import random
+
+
+# функция для печати содержимого листьев
+def printAllNodes(nodes,treeName):
+    print(treeName)
+    for node in nodes:
+        tmpNode = BSTNode(None,None)
+        tmpNode.NodeKey = node.NodeKey
+        tmpNode.LeftChild = node.LeftChild
+        tmpNode.RightChild = node.RightChild
+        tmpNode.Parent = node.Parent
+        tmpNode.Level = node.Level
+        if tmpNode.Parent is None:
+            tmpNode.Parent = BSTNode(None,None)
+        if tmpNode.LeftChild is None:
+            tmpNode.LeftChild = BSTNode(None,None)
+        if tmpNode.RightChild is None:
+            tmpNode.RightChild = BSTNode(None,None)
+        print('Node Key: ' + str(tmpNode.NodeKey) + ' Left ChildKey: ' + str(tmpNode.LeftChild.NodeKey)
+              + ' Right ChildKey: ' + str(tmpNode.RightChild.NodeKey) + ' Parent NodeKey: '+str(tmpNode.Parent.NodeKey)
+              + ' Node Level: ' + str(tmpNode.Level))
+    print('\n')
 
 a = [] # not full: 1 element in the left subtree , 1 in the right
 b = [] # full
@@ -15,7 +38,7 @@ random.shuffle(b)
 random.shuffle(c)
 print('a: '+str(a))
 print('b: '+str(b))
-print('b: '+str(c))
+print('c: '+str(c))
 
 a_out = GenerateBBSTArray(a)
 print('a_out :'+str(a_out))
@@ -23,3 +46,19 @@ b_out = GenerateBBSTArray(b)
 print('b_out :'+str(b_out))
 c_out = GenerateBBSTArray(c)
 print('c_out :'+str(c_out))
+
+a_tree = BalancedBST()
+b_tree = BalancedBST()
+c_tree = BalancedBST()
+
+a_tree.GenerateTree(a)
+b_tree.GenerateTree(b)
+c_tree.GenerateTree(c)
+
+printAllNodes(a_tree.getAllNodes(), 'a tree')
+printAllNodes(b_tree.getAllNodes(), 'b tree')
+printAllNodes(c_tree.getAllNodes(), 'c tree')
+
+print(a_tree.IsBalanced(a_tree.Root))
+print(b_tree.IsBalanced(b_tree.Root))
+print(c_tree.IsBalanced(c_tree.Root))
