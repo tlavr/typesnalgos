9157f43cc22b6ffec58e76014e86a069b73e6b15
Timur Lavrukhin <45234552+tlavr@users.noreply.github.com>
2020-01-22 17:19:04 +0300

    changed tests using unittest

--- heap_tests.py
+++ heap_tests.py
@@ -1,29 +1,39 @@
+import unittest
 from heap import *
 
-def print_heap(h,s):
-    print(s + ' heap elements: ')
-    i = 0
-    for key in h:
-        print(str(i)+' : '+str(key))
-        i += 1
+class testHeap(unittest.TestCase):
 
-testHeap = Heap()
-print_heap(testHeap.HeapArray,'Empty')
+    def testMakeFull(self):
+        self.testHeap = Heap()
+        a = []
+        d = 2
+        for key in range(1,pow(2, d + 1)):
+            a.append(key)
+        self.testHeap.MakeHeap(a,d)
+        self.assertEqual(self.testHeap.HeapArray,[7,6,5,4,3,2,1],'Full heap test')
 
-a = []
-d = 3
-for key in range(pow(2,d+1)-1):
-    a.append(key)
-testHeap.MakeHeap(a,d)
-print_heap(testHeap.HeapArray,'Full')
-print('Max: '+str(testHeap.GetMax()))
-print_heap(testHeap.HeapArray,'Full')
+    def testMakeEmpty(self):
+        self.testHeap = Heap()
+        a = []
+        d = 0
+        self.testHeap.MakeHeap(a,d)
+        self.assertEqual(self.testHeap.HeapArray,[],'Empty test')
 
-testHeap = Heap()
-a = [11,9,4,7,8,3,1,2,5,6]
-testHeap.MakeHeap(a,3)
-print_heap(testHeap.HeapArray,'Half full')
-print('Max: '+str(testHeap.GetMax()))
-testHeap.Add(10)
-testHeap.Add(12)
-print_heap(testHeap.HeapArray,'Half full')
+    def testAdd(self):
+        self.testHeap = Heap()
+        a = [11, 8, 6, 5, 4]
+        self.testHeap.MakeHeap(a, 2)
+        self.testHeap.Add(12)
+        self.assertEqual(self.testHeap.HeapArray, [12, 8, 11, 5, 4, 6, None], 'Add element test')
+
+    def testGetMax(self):
+        self.testHeap = Heap()
+        a = []
+        d = 2
+        for key in range(1,pow(2, d + 1)):
+            a.append(key)
+        self.testHeap.MakeHeap(a,d)
+        self.assertEqual(self.testHeap.GetMax(),7,'get max test')
+        self.assertEqual(self.testHeap.HeapArray, [6, 4, 5, 1, 3, 2, None], 'Full heap test')
+
+unittest.main(verbosity=2)
